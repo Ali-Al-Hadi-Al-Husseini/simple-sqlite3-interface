98416f2c36b65609d9d3164e54487cc21fd7e259
Ali-Al-Hadi-Al-Husseini <77203019+Ali-Al-Hadi-Al-Husseini@users.noreply.github.com>
2021-03-16 17:31:52 +0000

    auto completetion

---
+++ easy_to_use.py
@@ -0,0 +1,52 @@
+import sqlite3
+import readline
+from os import system, name
+from completer import Completer
+
+
+def main():
+    clear_terminal()
+    print('Welcome to easy_use V ALPHA')
+    print('.help for help')
+    data_base_name = input('Data-base-name:')
+    conection = sqlite3.connect(f'{data_base_name}.db')
+    cursor = conection.cursor()
+
+    completion = ["SELECT", "FROM", "CREATE", "TABLE",
+                  "INSERT", 'INTO', 'VALUES', 'WHERE', '!STOP!']
+    completer = Completer(completion)
+    readline.set_completer(completer.complete)
+    readline.parse_and_bind('tab: complete')
+
+    while True:
+        Command = str(input(">>>"))
+        if Command == '.exit':
+            conection.close()
+            exit()
+        elif Command == '.help':
+            print('hola mi amigo!')
+        elif Command == 'clear':
+            clear_terminal()
+
+        else:
+            try:
+                cursor.execute(Command)
+                conection.commit()
+
+            except Exception as error:
+                print(error)
+
+        if 'SELECT' in Command.upper():
+            for row in cursor.fetchall():
+                print(row)
+
+
+def clear_terminal():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
+
+
+if __name__ == "__main__":
+    main()
